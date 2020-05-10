7c154f7fc796fb81714d872c52dfcfb5b70a670b
Jeff Sinclair <jsinclair@atlassian.com>
2020-05-09 21:39:47 -0700

    typo in connection string

--- projects/01_fyyur/config.py
+++ projects/01_fyyur/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 DEBUG = True
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgres://fyyur@locahost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://fyyur@localhost:5432/fyyur'
 
 # Disable performance warnings
 SQLALCHEMY_TRACK_MODIFICATIONS = False
