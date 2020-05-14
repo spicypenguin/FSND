1fccd178ab116e6b1eb5f8a91634749319c265a8
Jeff Sinclair <jsinclair@atlassian.com>
2020-05-13 20:10:08 -0700

    added shows/ get endpoint

--- projects/01_fyyur/app.py
+++ projects/01_fyyur/app.py
@@ -10,6 +10,7 @@ from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 import logging
+from datetime import datetime
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
@@ -345,44 +346,16 @@ def create_artist_submission():
 @app.route('/shows')
 def shows():
     # displays list of shows at /shows
-    # TODO: replace with real venues data.
-    #       num_shows should be aggregated based on number of upcoming shows per venue.
+    shows = Show.query.all()
     data = [{
-        "venue_id": 1,
-        "venue_name": "The Musical Hop",
-        "artist_id": 4,
-        "artist_name": "Guns N Petals",
-        "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-        "start_time": "2019-05-21T21:30:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 5,
-        "artist_name": "Matt Quevedo",
-        "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-        "start_time": "2019-06-15T23:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-01T20:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-08T20:00:00.000Z"
-    }, {
-        "venue_id": 3,
-        "venue_name": "Park Square Live Music & Coffee",
-        "artist_id": 6,
-        "artist_name": "The Wild Sax Band",
-        "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-        "start_time": "2035-04-15T20:00:00.000Z"
-    }]
+        'venue_id': x.venue_id,
+        'venue_name': x.venue.name,
+        'artist_id': x.artist_id,
+        'artist_name': x.artist.name,
+        'artist_image_link': x.artist.image_link,
+        'start_time': datetime.strftime(x.start_time, '%Y-%m-%dT%H:%M:%S.%fZ')
+    } for x in shows]
+
     return render_template('pages/shows.html', shows=data)
 
 
@@ -399,7 +372,15 @@ def create_show_submission():
     # TODO: insert form data as a new Show record in the db, instead
 
     # on successful db insert, flash success
-    flash('Show was successfully listed!')
+    try:
+        new_show = Show(artist_id=1, venue_id=2, start_time='')
+        db.session.add(new_show)
+        flash('Show was successfully listed!')
+    except:
+        db.session.rollback()
+        flash('An error occured. Show could not be listed.', 'error')
+    finally:
+        db.session.close()
     # TODO: on unsuccessful db insert, flash an error instead.
     # e.g., flash('An error occurred. Show could not be listed.')
     # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
